7ef2b40751ab2ae5ab78e2a865d6bbc5903fe701
Carlos Coronado <carloscoronado098@gmail.com>
2021-03-25 19:54:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patrones_Proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
