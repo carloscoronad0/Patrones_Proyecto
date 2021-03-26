efa726e7b3bb0aa1fae564c94a784973b56fbb08
fabrifj <fabrifj@gmail.com>
2021-03-26 18:50:05 -0400

    instancias

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Patrones_Proyecto.Chain_of_Responsability;
+using Patrones_Proyecto.Observer;
 using System;
 
 namespace Patrones_Proyecto
@@ -11,7 +12,14 @@ namespace Patrones_Proyecto
             HandlerJefe jefe = new HandlerJefe(director);
             HandlerRevisor revisor = new HandlerRevisor(jefe);
             HandlerEditor editor = new HandlerEditor(revisor);
-            
+
+
+            Provincia arque = new Provincia("Arque");
+            Provincia mizque = new Provincia("Mizque");
+            Provincia arani = new Provincia("Arani");
+            Provincia Carrasco = new Provincia("Carrasco");
+            Provincia tiraque = new Provincia("Tiraque");
+
         }
     }
 }
